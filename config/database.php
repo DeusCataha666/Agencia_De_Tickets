6c19f0732ebae40a6dc9fa2c33083e9000a5261e
Polo <jesuspolosumalave@gmail.com>
2026-06-10 02:35:38 -0500

    fix: usar php cli con artisan servev2

--- config/database.php
+++ config/database.php
@@ -45,9 +45,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            //'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => '3306',
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
