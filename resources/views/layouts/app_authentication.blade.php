2d90d98f34675ff55a1ad9bc803247f6e6cf3309
Polo <jesuspolosumalave@gmail.com>
2026-06-11 12:27:23 -0500

    Fix Tailwind config path and trust proxies for HTTPS CSRF on Railway

--- resources/views/layouts/app_authentication.blade.php
+++ resources/views/layouts/app_authentication.blade.php
@@ -18,7 +18,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @stack('css')
 
